fe06b6aea3b80d6bbf35675c75fa77a1b0f34e86
zcf794743 <65210053+zcf794743@users.noreply.github.com>
2026-03-13 13:57:32 +0800

    Update __init__.py

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -44,7 +44,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.3.2"
+    plugin_version = "9.3.2"
     # 插件作者
     plugin_author = "mrtian2016"
     # 作者主页
